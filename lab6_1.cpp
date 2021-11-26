9c2cb040de3d98640769c30406d50ffbba62ad80
Elaina-AFK <khglhk@yahoo.com>
2021-11-26 16:04:06 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x;
+    int a = 0;
+    int b = 0;
+    while (x != 0){
+        cout << "Enter an integer: ";
+        cin >> x;
+        if(x % 2 == 1){
+            a++;
+        }else{
+            b++;
+        }
+    }
+    cout << "#Even numbers = " << b - 1 << endl;
+    cout << "#Odd numbers = " << a;
     return 0;
 }
